70d8994d220d50ee8a02291c5b5a10a4df9e128c
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 03:46:24 +0700

    rollback to using vertical style in flatlist

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,24 +3,24 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const  ListScreen = () => {
   const friends = [
-    { name: 'Friend #1', key: '1' },
-    { name: 'Friend #2', key: '2' },
-    { name: 'Friend #3', key: '3' },
-    { name: 'Friend #4', key: '4' },
-    { name: 'Friend #5', key: '5' },
-    { name: 'Friend #6', key: '6' },
-    { name: 'Friend #7', key: '7' },
-    { name: 'Friend #8', key: '8' },
-    { name: 'Friend #9', key: '9' },
+    { name: 'Friend #1', key: '1', age: 20 },
+    { name: 'Friend #2', key: '2', age: 21 },
+    { name: 'Friend #3', key: '3', age: 22 },
+    { name: 'Friend #4', key: '4', age: 23 },
+    { name: 'Friend #5', key: '5', age: 24 },
+    { name: 'Friend #6', key: '6', age: 25 },
+    { name: 'Friend #7', key: '7', age: 26 },
+    { name: 'Friend #8', key: '8', age: 27 },
+    { name: 'Friend #9', key: '9', age: 28 },
   ];
 
   return <FlatList 
-    horizontal
-    showsHorizontalScrollIndicator={false}
     keyExtractor={friend => friend.name}
     data={friends} 
     renderItem={({ item }) => {
-      return <Text style={styles.textStyle}>{item.name}</Text>
+      return <Text style={styles.textStyle}>
+        {item.name} - Age {item.age}
+      </Text>
     }}
   />
 };
