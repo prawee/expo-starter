40c8624515dba2b8cdc32dccd21ca54d005a329f
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 14:55:09 +0700

    update parent data to child

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = (props) => {
+const ImageDetail = ({ title, imageSource, score }) => {
   return (
     <View>
-      <Image source={require('../../assets/beach.jpg')} />
-      <Text>{props.title}</Text>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image Score - {score}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default ImageDetail;
