40f326a6babf6b7779c9a789e85af75088063054
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 16:28:11 +0700

    send props with action to child

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,11 +6,21 @@ const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+
+  console.log(red);
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Blue" />
-      <ColorCounter color="Green" />
+      <ColorCounter 
+        onIncrease={()=> setRed(red +1)}
+        onDecrease={()=> setRed(red -1)}
+        color="Red" 
+      />
+      <ColorCounter 
+        color="Blue" 
+      />
+      <ColorCounter 
+        color="Green" 
+      />
     </View>
   )
 }
