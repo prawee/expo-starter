f2cda797aa4810517bdb987782994374efd35d29
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 15:14:21 +0700

    add button to increase and decrease of counter

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CounterScreen = () => {
+  let counter = 0;
+  return (
+    <View>
+      <Button 
+        title="Increase" 
+        onPress={() => {
+          counter++;
+          console.log(counter);
+        }} />
+      <Button 
+        title="Decrease" 
+        onPress={() => {
+          counter--;
+          console.log(counter);
+        }} />
+      <Text>Current Count: {counter}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
