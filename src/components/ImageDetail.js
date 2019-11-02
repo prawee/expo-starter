5a8357a2f070d41b49da6f20888cade4f1c42021
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 13:49:29 +0700

    create image detail comp

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageDetail = (props) => {
+  // console.log(props);
+  return <Text>{props.title}</Text>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ImageDetail;
