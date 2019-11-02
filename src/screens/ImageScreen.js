ee5b139ca541b0f973f568c9a7c8d909b30c5055
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 13:10:17 +0700

    create image screen and register it

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageScreen = () => {
+  return <Text>Image Screeen</Text>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ImageScreen;
