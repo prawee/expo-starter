ad0d6148d3356cd3fb924c6aa982b747bc8fcf22
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 16:22:53 +0700

    using comp for each color

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
   return (
     <View>
-      <Text>Square Screen</Text>
+      <ColorCounter color="Red" />
+      <ColorCounter color="Blue" />
+      <ColorCounter color="Green" />
     </View>
   )
 }
