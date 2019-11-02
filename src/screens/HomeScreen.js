57325b516bc4248c25ea1833c1a0fb9edfba5db5
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 11:31:16 +0700

    remove tuchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -12,13 +12,6 @@ const HomeScreen = (props) => {
       title="Go to List Demo"
       onPress={() => props.navigation.navigate('List')}
     />
-    <TouchableOpacity
-      onPress={() => props.navigation.navigate('List')}
-    >
-      <Text>Go to List Demo</Text>
-      <Text>Go to List Demo</Text>
-      <Text>Go to List Demo</Text>
-    </TouchableOpacity>
   </View>;
 };
 
