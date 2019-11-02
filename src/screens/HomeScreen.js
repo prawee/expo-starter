ea0ef4397d98c9633b979df7fe14786f376384f6
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 10:33:09 +0700

    try testing button and action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+  return <View>
+    <Text style={styles.text}>Hi there!</Text>
+    <Button 
+      onPress={() => console.log('button pressd!')}
+      title="Go to Components Demo" />
+  </View>;
 };
 
 const styles = StyleSheet.create({
