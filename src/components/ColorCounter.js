36de0ed5fda433337578445ab4c52ae931d29b4b
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 16:28:39 +0700

    action to call parent function

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
       <Button 
-        title={`Increase ${color}`} />
+        title={`Increase ${color}`} 
+        onPress={() => onIncrease()}
+      />
       <Button 
-        title={`Decrease ${color}`} />
+        title={`Decrease ${color}`} 
+        onPress={() => onDecrease()}
+      />
     </View>
   )
 }
