1be786db00f2363bc38e523d61d37a855f761bc4
Prawee Wongsa <prawee@hotmail.com>
2019-11-02 17:10:05 +0700

    update setColor with green and blue logic

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -16,6 +16,12 @@ const SquareScreen = () => {
       case 'red':
         red + change > 255 || red + change < 0 ? null : setRed(red + change);
         return;
+      case 'green':
+        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+        return;
+      case 'blue':
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+        return;
     }
   };
 
