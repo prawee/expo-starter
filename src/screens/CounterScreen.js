3d1332a69d8106d02a31e3d2cb1805b906bbbd23
Prawee Wongsa <prawee@hotmail.com>
2019-11-03 14:18:56 +0700

    update using useReducer instead useState

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+
+const reducer = (state, action) => {
+  // state === { count: number }
+  // action === { type: 'increment' || 'decrement', payload: 1 }
+  switch(action.type) {
+    case 'increment':
+      return {...state, count: state.count + action.payload };
+    case 'decrement':
+      return {...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 
 const CounterScreen = () => {
   //let counter = 0;
-  const [counter, setCounter] = useState(0);
+  //const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <View>
       <Button 
@@ -11,16 +25,18 @@ const CounterScreen = () => {
         onPress={() => {
           //counter++;
           //console.log(counter);
-          setCounter(counter+1);
+          //setCounter(counter+1);
+          dispatch({ type: 'increment', payload: 1 })
         }} />
       <Button 
         title="Decrease" 
         onPress={() => {
           //∂counter--;
           //console.log(counter);
-          setCounter(counter-1);
+          //setCounter(counter-1);
+          dispatch({ type: 'decrement', payload: 1 })
         }} />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {state.count}</Text>
     </View>
   );
 };
