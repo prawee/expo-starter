d6188ea8498bf714b5561709ab0b2756c908494d
Prawee Wongsa <prawee@hotmail.com>
2019-11-06 21:58:13 +0700

    example for using box systems

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,24 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Box Screen</Text>
     </View>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black'
+  },
+  textStyle: {
+    borderWidth: 10,
+    borderColor: 'red',
+    //marginVertical: 20,
+    //marginHorizontal: 20
+    margin: 20
+  }
+});
 
 export default BoxScreen;
